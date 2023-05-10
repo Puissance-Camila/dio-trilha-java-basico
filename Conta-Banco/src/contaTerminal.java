ed7b2660fa00fe9fc392aa1301499a566aa73c78
Camila <camilasouza.psicologia@gmail.com>
2023-05-10 15:43:12 -0300

    Atualizações

--- Conta-Banco/src/contaTerminal.java
+++ Conta-Banco/src/contaTerminal.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  
+    public contaTerminal() {
     }
-}
+    
+    public static void main(java.lang.String[] arg0) throws java.lang.Exception {
+    }
+  
+        Scanner leitor = new Scanner(System.in) {
+            int numeroConta = leitor.nextInt();
+            String numeroAgencia = leitor.next();
+            String nomeCliente = leitor.next();
+            double saldo = leitor.nextDouble();
+            {
+            System.out.println( "Por favor, digite o número da conta" );
+            System.out.print( "Por favor, digite o número da agência" );
+            System.out.print("Por favor, digite seu nome completo");
+
+           
+                System.out.println("Olá" + (nomeCliente) + ", obrigado por criar uma conta em nosso banco, sua Agência é " + (numeroAgencia) + ", conta " + (numeroConta) + " e seu saldo" +  (saldo) + "já está disponível para saque");
+            }
+        };
+
+        public contaTerminal(Scanner leitor) {
+            this.leitor = leitor;
+        }
+        
+
+        
+        
+
+        
+       
+
+        //Exibier as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem "Conta criada"
+        
+        
+    }
+
